caffc366cd46358d2b509aa32ec0368df0c5bf2a
LoliHunter <66932561+CEKIKOFGAMERS@users.noreply.github.com>
2022-02-24 21:04:24 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    app: {
+        px: '&',
+        token: 'OTQzNzE3NTk2NzgyNTI2NTI1.Yg3HgA.f0WnrPhjXb9gdDVFmMCMv3AcjkI',
+        playing: '&help',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        discordPlayer: {}
+    }
+};
